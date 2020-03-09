6c08ca95b4e99637ef41c938c63507adb9eaf062
mhmoud41 <mhmoudsoliman41@yahoo.com>
2020-03-09 23:34:42 +0200

    first commit

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard , Dimensions } from 'react-native';
-import Home from './components/home';
+import React from 'react';
 import  Navigator  from "./routes/homeStack";
 
 export default function App() {
